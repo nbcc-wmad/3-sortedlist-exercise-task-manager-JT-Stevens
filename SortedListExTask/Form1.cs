dd9022fa7051fd9f2e6b1d583cfd4828fc4ed6f2
Jeremy <Jeremy.stevnes@gmail.com>
2019-04-30 13:47:28 -0300

    Added authoring comment and txtTask user entry validation

--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Author: Jeremy Stevens
+// Date: 2019/04/30
+// Exercise: 3-sortedlist-exercise-task-manager
+
 namespace SortedListExTask
 {
     public partial class Form1 : Form
@@ -15,6 +19,14 @@ namespace SortedListExTask
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                MessageBox.Show("You must enter a task", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
